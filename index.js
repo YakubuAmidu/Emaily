e8b213e5a8e40ae98b73eaa15f39f385bf255616
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-11-01 16:20:45 -0700

    Initial commit

--- index.js
+++ index.js
@@ -9,13 +9,21 @@ passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
-}, (accessToken) => {
-    console.log(accessToken);
+}, (accessToken, refreshToken, profile, done) => {
+    console.log('access token', accessToken);
+    console.log('refresh token', refreshToken);
+    console.log('profile', profile);
 }));
 
-app.get('/', (req, res) => {
-    res.send({ bye: 'buddy' });
-});
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+// app.get('/', (req, res) => {
+//     res.send({ bye: 'buddy' });
+// });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, (req, res) => {
